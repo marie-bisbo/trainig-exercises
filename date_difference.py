4d9c124eb7abd70d788179225b8f9e2e7b924561
marie-brainlabs <marie@brainlabsdigital.com>
2020-02-21 09:55:07 +0000

    Add try except

--- date_difference.py
+++ date_difference.py
@@ -1,11 +1,15 @@
 from dateutil import parser, relativedelta
+from dateutil.parser import ParserError
 
 
 def date_difference(date_1: str, date_2: str) -> str:
-    date_difference = relativedelta.relativedelta(
-        parser.parse(date_2, dayfirst=True), parser.parse(date_1, dayfirst=True)
-    )
-    return f"{date_difference.years} years, {date_difference.months} months, {date_difference.days} days"
+    try:
+        date_difference = relativedelta.relativedelta(
+            parser.parse(date_2, dayfirst=True), parser.parse(date_1, dayfirst=True)
+        )
+        return f"{date_difference.years} years, {date_difference.months} months, {date_difference.days} days"
+    except ParserError:
+        return "Error computing the date difference. Please input a valid date format."
 
 
-print(date_difference("01/01/2020", "13/02/2020"))
+print(date_difference("1st Jan 2020", "2nd Feb 2019"))
