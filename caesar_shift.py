aa5d849a5599ff2e4f7c1eb5beeb00ca46549a88
marie-brainlabs <marie@brainlabsdigital.com>
2020-02-21 09:38:08 +0000

    Split into mutliple functions

--- caesar_shift.py
+++ caesar_shift.py
@@ -1,28 +1,31 @@
 import string
 
-lowercase_alphabet = list(string.ascii_lowercase)
-uppercase_alphabet = list(string.ascii_uppercase)
+LOWERCASE_ALPHABET = list(string.ascii_lowercase)
+UPPERCASE_ALPHABET = list(string.ascii_uppercase)
 
 
 def encode(message: str, shift: int) -> str:
-    shifted_message = ""
-    for letter in message:
-        shifted_message += (
-            shift_letter(letter, shift, lowercase_alphabet)
-            if letter in lowercase_alphabet
-            else shift_letter(letter, shift, uppercase_alphabet)
-            if letter in uppercase_alphabet
-            else letter
-        )
-    return shifted_message
-
-
-def shift_letter(letter: str, shift: int, alphabet: list):
-    return alphabet[(alphabet.index(letter) + shift) % 26]
+    encoded_message = "".join((shift_letter(letter, shift) for letter in message))
+    return encoded_message
 
 
 def decode(encoded_message: str, shift: int) -> str:
     return encode(encoded_message, -shift)
+
+
+def shift_letter(letter: str, shift: int) -> str:
+    letter = (
+        check_case(letter, shift, LOWERCASE_ALPHABET)
+        if letter in LOWERCASE_ALPHABET
+        else check_case(letter, shift, UPPERCASE_ALPHABET)
+        if letter in UPPERCASE_ALPHABET
+        else letter
+    )
+    return letter
+
+
+def check_case(letter: str, shift: int, alphabet: list) -> str:
+    return alphabet[(alphabet.index(letter) + shift) % 26]
 
 
 print(encode("This is a message!", 1))
